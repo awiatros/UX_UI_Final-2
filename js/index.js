09d887b3d6eca830696869f0475e4590164ea7f4
awiatros <90422853+awiatros@users.noreply.github.com>
2022-03-10 18:59:04 -0600

    Dashboard_Final

--- js/index.js
+++
@@ -1,9 +0,0 @@
-$(".btn-week").on("click", function(){
-    $(".cal-week").removeClass("hide-cal");
-    $(".cal-month").addClass("hide-cal");
-});
-
-$(".btn-month").on("click", function(){
-    $(".cal-month").removeClass("hide-cal");
-    $(".cal-week").addClass("hide-cal");
-});
